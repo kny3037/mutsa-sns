31be2da3e35a77c17ff228b1e3ca7112b2831bb4
nayeong kim <psi5059@naver.com>
2022-12-20 13:04:14 +0900

    first commit

---
+++ src/main/java/com/mutsasnskimnayeong/MutsaSnsKimnayeongApplication.java
@@ -0,0 +1,13 @@
+package com.mutsasnskimnayeong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutsaSnsKimnayeongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutsaSnsKimnayeongApplication.class, args);
+	}
+
+}
