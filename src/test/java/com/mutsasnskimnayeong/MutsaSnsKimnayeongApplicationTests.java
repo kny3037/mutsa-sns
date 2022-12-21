87a67c3009cebb25d35d7707c4bb2037a693714f
nayeong kim <psi505968@gmail.com>
2022-12-21 15:06:21 +0900

    first commit

---
+++ src/test/java/com/mutsasnskimnayeong/MutsaSnsKimnayeongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mutsasnskimnayeong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutsaSnsKimnayeongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
