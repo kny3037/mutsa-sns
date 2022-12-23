7084f3a323ebf76a3c03cc4826c865ae0070242e
nayeong kim <psi505968@gmail.com>
2022-12-23 14:24:12 +0900

    Post create 작성

---
+++ src/main/java/com/mutsasnskimnayeong/controller/PostController.java
@@ -0,0 +1,30 @@
+package com.mutsasnskimnayeong.controller;
+
+import com.mutsasnskimnayeong.domain.dto.PostCreateRequest;
+import com.mutsasnskimnayeong.domain.dto.PostCreateResponse;
+import com.mutsasnskimnayeong.domain.dto.PostDto;
+import com.mutsasnskimnayeong.domain.response.Response;
+import com.mutsasnskimnayeong.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
+
+@RestController
+@RequestMapping("/api/v1/posts")
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostService postService;
+
+    @PostMapping
+    public Response<PostCreateResponse> create(@RequestBody PostCreateRequest createRequest, @ApiIgnore Authentication authentication){
+        String userName = authentication.getName();
+        PostDto postDto = postService.create(createRequest, userName);
+
+        return Response.success(new PostCreateResponse("포스트 등록 완료",postDto.getId()));
+    }
+}
---
+++ src/test/java/com/mutsasnskimnayeong/controller/PostControllerTest.java
@@ -0,0 +1,10 @@
+package com.mutsasnskimnayeong.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest
+class PostControllerTest {
+
+}
