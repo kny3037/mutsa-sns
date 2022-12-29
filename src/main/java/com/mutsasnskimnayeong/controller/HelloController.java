bed5e169bfff998026cbda8a3b2b01932183a88c
nayeong kim <psi505968@gmail.com>
2022-12-29 09:46:16 +0900

    자릿 수 합 구하기

--- src/main/java/com/mutsasnskimnayeong/controller/HelloController.java
+++ src/main/java/com/mutsasnskimnayeong/controller/HelloController.java
@@ -1,15 +1,26 @@
 package com.mutsasnskimnayeong.controller;
 
+import com.mutsasnskimnayeong.service.AlgorithmService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1")
+@RequiredArgsConstructor
 public class HelloController {
+
+    private final AlgorithmService algorithmService;
 
     @GetMapping("/hello")
     public String hello(){
         return "김나영";
+    }
+
+    @GetMapping("/hello/{num}")
+    public Integer sum(@PathVariable Integer num){
+        return algorithmService.SumOfDigit(num);
     }
 }
---
+++ src/main/java/com/mutsasnskimnayeong/service/AlgorithmService.java
@@ -0,0 +1,18 @@
+package com.mutsasnskimnayeong.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AlgorithmService {
+
+    public Integer SumOfDigit(Integer num){
+
+        int sum = 0;
+
+        while (num > 0){
+            sum += num%10;
+            num/=10;
+        }
+        return sum;
+    }
+}
