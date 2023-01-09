5f8286c801f18ca1b921f4ed8bacef49492cb6c5
nayeong kim <psi505968@gmail.com>
2023-01-09 09:54:12 +0900

    like deleted_at @Where 추가

--- src/main/java/com/mutsasnskimnayeong/domain/entity/Like.java
+++ src/main/java/com/mutsasnskimnayeong/domain/entity/Like.java
@@ -2,6 +2,7 @@ package com.mutsasnskimnayeong.domain.entity;
 
 import lombok.*;
 import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -12,7 +13,8 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "\"likes\"")
-/*@SQLDelete(sql = "UPDATE \"likes\" SET delete_at = current_timestamp where id = ?")*/
+@SQLDelete(sql = "UPDATE \"likes\" SET delete_at = current_timestamp where id = ?")
+@Where(clause = "deleted_at is null")
 public class Like extends BaseEntity{
 
     @Id
